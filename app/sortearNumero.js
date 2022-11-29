dac065c80e71ce1b93fdaff18eaf90c4560dfcae
mend1s <andremendesskt99@gmail.com>
2022-11-29 18:46:26 -0300

    configurando função de sorteio de números aleatórios

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,7 +1,16 @@
+const menorValor = 1
+const maiorValor = 1000
+
 const numeroSecreto = gerarNumeroSecreto()
 
 function gerarNumeroSecreto() {
-    return parseInt(Math.random() * 100)
+    return parseInt(Math.random() * maiorValor + 1)
 }
 
 console.log('Número sorteado:', numeroSecreto)
+
+const elementoMenor = document.getElementById('menor-valor')
+elementoMenor.innerHTML = menorValor
+
+const elementoMaior = document.getElementById('maior-valor')
+elementoMaior.innerHTML = maiorValor
