55209cf7681cb3c73ad7020f6abbf88844a08b1c
mend1s <andremendesskt99@gmail.com>
2022-11-29 19:20:01 -0300

    configurando função de voz na tela

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,20 @@
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const elementoChute = document.getElementById('chute')
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-BR'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+    const chute = e.results[0][0].transcript 
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
+}
